0f88206920fd55bda6450a5106dce75765399927
Martin Grotzke <martin.grotzke@googlemail.com>
2013-12-01 02:40:10 +0100

    Add objenesis sample

---
+++ objenesis/src/main/java/io/ino/kryo/osgisamples/objenesis/Activator.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2013 Martin Grotzke (http://ino.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.ino.kryo.osgisamples.objenesis;
+
+import org.objenesis.strategy.StdInstantiatorStrategy;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
+public class Activator implements BundleActivator {
+    
+	private Kryo kryo;
+
+    public void start(BundleContext context) throws Exception {
+		kryo = new Kryo();
+		kryo.setInstantiatorStrategy(new StdInstantiatorStrategy());
+
+		try {
+			roundTrip(new ClassWithoutDefaultConstructor("foo"));
+		} catch(Throwable e) {
+			e.printStackTrace();
+		}
+    }
+
+	private void roundTrip(Object obj) {
+		Object deserialized = deserialize(serialize(obj), obj.getClass());
+		System.out.println("Serializing "+ obj.getClass().getSimpleName() +": " + (deserialized.equals(obj) ? "OK" : "Failed"));
+	}
+
+    public void stop(BundleContext context) throws Exception {
+    }
+
+    private byte[] serialize(final Object o) {
+        if ( o == null ) {
+            throw new NullPointerException( "Can't serialize null" );
+        }
+
+        final Output output = new Output(4096);
+        kryo.writeObject(output, o);
+        output.flush();
+        return output.getBuffer();
+    }
+
+    private <T> T deserialize(final byte[] in, final Class<T> clazz) {
+        final Input input = new Input(in);
+        return kryo.readObject(input, clazz);
+    }
+
+}
---
+++ objenesis/src/main/java/io/ino/kryo/osgisamples/objenesis/ClassWithoutDefaultConstructor.java
@@ -0,0 +1,47 @@
+package io.ino.kryo.osgisamples.objenesis;
+
+public class ClassWithoutDefaultConstructor {
+	
+    final String value;
+
+    public ClassWithoutDefaultConstructor( final String value ) {
+        this.value = value;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ( ( value == null )
+            ? 0
+            : value.hashCode() );
+        return result;
+    }
+
+    @Override
+    public boolean equals( final Object obj ) {
+        if ( this == obj ) {
+            return true;
+        }
+        if ( obj == null ) {
+            return false;
+        }
+        if ( getClass() != obj.getClass() ) {
+            return false;
+        }
+        final ClassWithoutDefaultConstructor other = (ClassWithoutDefaultConstructor) obj;
+        if ( value == null ) {
+            if ( other.value != null ) {
+                return false;
+            }
+        } else if ( !value.equals( other.value ) ) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassWithoutDefaultConstructor [value=" + value + "]";
+    }
+}
